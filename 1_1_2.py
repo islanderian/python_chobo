b5ece404a7d22624b764b358ed4f64f88c29f44d
islanderian <createdby.island@gmail.com>
2024-04-05 18:06:29 +0900

    1.6 파이썬 인터프리터

---
+++ 1_1_2.py
@@ -0,0 +1,9 @@
+"""1.1.2 연습문제:직선거리 계산"""
+import math
+
+to_hospital = 550
+to_burger = 600
+
+result = math.sqrt(to_hospital**2 + to_burger**2)
+
+print(result)
---
+++ 1_2_1.py
@@ -0,0 +1,6 @@
+r = 800
+t = 110
+
+file = r*t/1000
+
+print(file)
